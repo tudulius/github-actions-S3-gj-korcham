3cadb293319c2af5e258b68296a41959a2db8062
Tudulius <tudulius@gmail.com>
2025-10-27 09:55:01 +0900

    .

---
+++ gitpush.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# …or create a new repository on the command line
+# echo "# Github-test" >> README.md
+# git init
+git add .
+git commit -m "github-test"
+# git branch -M main
+# git remote add origin https://github.com/tudulius/Github-test.git
+git push -u origin main
+
+# …or push an existing repository from the command line
+# git remote add origin https://github.com/tudulius/Github-test.git
+# git branch -M main
+# git push -u origin main
